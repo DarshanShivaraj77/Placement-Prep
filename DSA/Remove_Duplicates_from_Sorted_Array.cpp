e54a6942ac6046013916b45854c7628a5cf6e23a
DarshanShivaraj77 <146374507+DarshanShivaraj77@users.noreply.github.com>
2025-06-12 10:59:45 +0530

    Update Remove_Duplicates_from_Sorted_Array.cpp

--- DSA/Remove_Duplicates_from_Sorted_Array.cpp
+++ DSA/Remove_Duplicates_from_Sorted_Array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i=1;
         if(nums.empty()) return 0;
-        for(int j=0;j<nums.size();j++){
+        for(int j=1;j<nums.size();j++){
             if(nums[j]!=nums[i-1]){
                 nums[i]=nums[j];
                 i++;
